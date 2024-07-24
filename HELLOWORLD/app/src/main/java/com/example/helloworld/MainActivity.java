a6619bc4e9f1a6c68b843599253c7a15ab2cfc41
bintangrpl <87853827+Vinobin@users.noreply.github.com>
2024-07-24 12:11:22 +0700

    VIDIO 3
    ANDROID STUDIO PAK ISA

--- HELLOWORLD/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HELLOWORLD/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -15,10 +15,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        tampil();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+    }
+    public void tampil(){
+        System.out.println("pusing");
     }
 }
