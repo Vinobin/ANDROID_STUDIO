96bf39a0ee7227d048343067bb36bbd37647a1b8
bintangrpl <87853827+Vinobin@users.noreply.github.com>
2024-07-25 07:58:08 +0700

    VIDEO 4
    ANDROID STUDIO PAK ISA

--- HELLOWORLD/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HELLOWORLD/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.helloworld;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View Lv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
