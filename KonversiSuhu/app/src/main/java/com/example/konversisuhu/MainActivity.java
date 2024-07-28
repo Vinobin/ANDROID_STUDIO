7afe73befa14e9af539aba98a2ced23dedfee6a9
bintangrpl <87853827+Vinobin@users.noreply.github.com>
2024-07-28 19:37:19 +0700

    VIDEO 10
    ANDROID STUDIO PAK ISA

--- KonversiSuhu/app/src/main/java/com/example/konversisuhu/MainActivity.java
+++ KonversiSuhu/app/src/main/java/com/example/konversisuhu/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.konversisuhu;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,29 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        load();
+        isiSpinner();
+    }
+    public void load(){
+        spinner = findViewById(R.id.spinner);
+    }
+
+    public void isiSpinner(){
+        String[] isi= {"Celcius to Reamur", "Celcius to Fahrenheit", "Celcius to Kelvin"};
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,isi);
+        spinner.setAdapter(adapter);
+    }
+
+    public void btnKonversi(View view) {
+        String pilihan = spinner.getSelectedItem().toString();
+        System.out.println(pilihan);
     }
 }
