c5bd5fadcf0ce0e00744daa0253a7d05feef6f2e
bintangrpl <87853827+Vinobin@users.noreply.github.com>
2024-07-28 20:22:17 +0700

    VIDEO 11
    ANDROID STUDIO PAK ISA

--- KonversiSuhu/app/src/main/java/com/example/konversisuhu/MainActivity.java
+++ KonversiSuhu/app/src/main/java/com/example/konversisuhu/MainActivity.java
@@ -5,7 +5,10 @@ import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     Spinner spinner;
+    EditText etNilai;
+    TextView TVHASIL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,20 +28,71 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         load();
-        isiSpinner();
+
     }
-    public void load(){
+
+    public void load() {
         spinner = findViewById(R.id.spinner);
+        etNilai = findViewById(R.id.etNilai);
+        TVHASIL = findViewById(R.id.TVHASIL);
     }
 
-    public void isiSpinner(){
-        String[] isi= {"Celcius to Reamur", "Celcius to Fahrenheit", "Celcius to Kelvin"};
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,isi);
-        spinner.setAdapter(adapter);
-    }
-
+    /*
+        public void isiSpinner(){
+            String[] isi= {"Celcius to Reamur", "Celcius to Fahrenheit", "Celcius to Kelvin"};
+            ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,isi);
+            spinner.setAdapter(adapter);
+        }
+    */
     public void btnKonversi(View view) {
         String pilihan = spinner.getSelectedItem().toString();
-        System.out.println(pilihan);
+        if (etNilai.getText().toString().equals("")) {
+            Toast.makeText(this, "gaboleh kosong ya", Toast.LENGTH_SHORT).show();
+        } else {
+            if (pilihan.equals("Celcius to Reamur")) {
+                cToR();
+            }
+            if (pilihan.equals("Celcius to Fahrenheit")) {
+                cTof();
+            }
+            if (pilihan.equals("Celcius to Kelvin")) {
+                cTok();
+            }
+            if (pilihan.equals("Reamur to Celcius")) {
+                rToc();
+            }
+            if (pilihan.equals("Reamur to Celcius")) {
+                rTof();
+            }
+        }
+
+    }
+
+    public void cToR() {
+        double suhu = Double.parseDouble(etNilai.getText().toString());
+        double hasil = (4.0 / 5.0) * suhu;
+        TVHASIL.setText(hasil + "");
+    }
+
+    public void cTof() {
+        double suhu = Double.parseDouble(etNilai.getText().toString());
+        double hasil = (9.0 / 5.0) * suhu + 32;
+        TVHASIL.setText(hasil + "");
+    }
+
+    public void cTok() {
+        double suhu = Double.parseDouble(etNilai.getText().toString());
+        double hasil = suhu + 273;
+        TVHASIL.setText(hasil + "");
+    }
+    public void rToc() {
+        double suhu = Double.parseDouble(etNilai.getText().toString());
+        double hasil = (5.0 / 4.0) * suhu;
+        TVHASIL.setText(hasil + "");
+    }
+    public void rTof() {
+        double suhu = Double.parseDouble(etNilai.getText().toString());
+        double hasil =(9.0 / 4.0) * suhu + 32;
+        TVHASIL.setText(hasil + "");
     }
 }
